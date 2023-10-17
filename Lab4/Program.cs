8050772ad33e013dfe8020f2a8fe30126baacdae
ozcnii <ercount89@gmail.com>
2023-10-17 10:17:55 +0400

    asdf

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,31 +1,124 @@
-﻿var main = () =>
+﻿var main = (Dictionary<string, Action> menuActions) =>
 {
-    // TODO: что в отчете писать, кроме кода?
+    Console.WriteLine("Лабораторная работа 4. Выполнил студент 6103 Фокин Евгений\n");
+    while (true)
+    {
+        Console.WriteLine("Выберите один из пунтов меню");
+        Console.WriteLine("0 - Осознанное завершение");
 
-    // Десятичный счетчик
-    var minValue = 15;
-    var maxValue = 20;
-    var counter = new DecimalCounter(minValue, maxValue, maxValue - minValue);
-    Console.WriteLine(counter.GetValue());
-    counter.Increase(5);
-    Console.WriteLine(counter.GetValue());
-    counter.Increase(1);
-    Console.WriteLine(counter.GetValue());
-    counter.Decrease(5);
-    Console.WriteLine(counter.GetValue());
-    counter.Decrease(3);
-    Console.WriteLine(counter.GetValue());
+        for (int index = 0; index < menuActions.Count; index++)
+        {
+            var item = menuActions.ElementAt(index);
+            Console.WriteLine($"{index + 1} - {item.Key}");
+        }
 
-    // Многочлен
-    var p1 = new Polynomial(1, 5, 6);
-    Console.WriteLine(p1.GetSolution());
-    var p2 = new Polynomial(1, -4, 4);
-    Console.WriteLine(p2.GetSolution());
-    var p3 = new Polynomial(1, 3, 9);
-    Console.WriteLine(p3.GetSolution());
+        string menuAction = Console.ReadLine()!.Trim();
+        bool parsed = int.TryParse(menuAction, out int actionIndex);
+
+        if (!parsed || 0 > actionIndex || actionIndex > menuActions.Count)
+            Console.WriteLine("Не выбран ни один пункт меню\n");
+        else if (actionIndex == 0)
+            return;
+        else
+        {
+            var element = menuActions.ElementAt(actionIndex - 1);
+            Console.WriteLine(element.Key + "\n");
+            element.Value();
+            Console.WriteLine("Все проверки прошли успешно\n");
+        }
+    }
 };
 
-main();
+main(new Dictionary<string, Action>{
+    {"Десятичный счетчик", () => {
+        Console.WriteLine("Введите минимально значение");
+        int minValue = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Введите максимальное значение");
+        int maxValue = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Введите начальное значение");
+        int initialValue = int.Parse(Console.ReadLine()!);
+
+        var counter = new DecimalCounter(minValue, maxValue, initialValue);
+
+        while (true)
+        {
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("0 - вернуться в меню");
+            Console.WriteLine("1 - увеличить значение");
+            Console.WriteLine("2 - уменьшить значение");
+            Console.WriteLine("3 - получить значение");
+
+            string selectedAction = Console.ReadLine()!.Trim();
+
+            switch (selectedAction)
+            {
+
+                case "0":
+                    return;
+
+                case "1":
+                    Console.WriteLine("Введите число, на которое нужно увеличить");
+                    counter.Increase(int.Parse(Console.ReadLine()!));
+                    Console.WriteLine(counter.GetValue());
+                    break;
+
+                case "2":
+                    Console.WriteLine("Введите число, на которое нужно уменьшить");
+                    counter.Decrease(int.Parse(Console.ReadLine()!));
+                    Console.WriteLine(counter.GetValue());
+                    break;
+
+                case "3":
+                    counter.GetValue();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }},
+    {"Многочлен", () => {
+
+        while (true)
+        {
+
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("0 - вернуться в меню");
+            Console.WriteLine("1 - ввести значения");
+
+            string selectedAction = Console.ReadLine()!.Trim();
+
+            switch (selectedAction)
+            {
+                case "0":
+                    return;
+
+                case "1":
+                    Console.WriteLine("Введите a");
+                    int a = int.Parse(Console.ReadLine()!);
+
+                    Console.WriteLine("Введите b");
+                    int b = int.Parse(Console.ReadLine()!);
+
+                    Console.WriteLine("Введите c");
+                    int c = int.Parse(Console.ReadLine()!);
+
+                    var polynomial = new Polynomial(a, b, c);
+                    Console.WriteLine(polynomial.GetSolution() + "\n");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        // var p1 = new Polynomial(1, 5, 6);
+        // Console.WriteLine(p1.GetSolution());
+        // var p2 = new Polynomial(1, -4, 4);
+        // Console.WriteLine(p2.GetSolution());
+        // var p3 = new Polynomial(1, 3, 9);
+        // Console.WriteLine(p3.GetSolution());
+    }}
+});
 
 class DecimalCounter
 {
